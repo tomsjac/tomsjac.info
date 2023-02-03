0c99afb35d57ec868be401ca8b6384f4f8a79c19
Thomas JACQUEY <thomas@webcd.fr>
2023-02-03 18:18:06 +0100

    feat: Loader component

--- components/partials/loader.vue
+++ components/partials/loader.vue
@@ -1,22 +1,40 @@
 <template>
-    <div id="loader" style="transform: translate(0px, -100%); opacity: 0; display: none;">
+    <div id="loader">
         <span class="color">Thomas</span>Jacquey 
     </div>
 </template>
 
-<style>
-#loader{
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    flex-direction: column;
-    height:100vh;
-    position: fixed;
-    z-index:100;
-    font-family: poppins;
-    background-color: #202020;
-    color: whitesmoke;
-    font-size: 70px;
-    width: 100%;
-}
+
+<script setup lang="ts">
+  import gsap from "~/node_modules/gsap";
+
+  onMounted(() => {
+    setTimeout(function(){
+      gsap.to('#loader',1,{y:"-100%"});
+      gsap.to('#loader',1,{opacity:0});
+      gsap.to('#loader',0,{display:"none",delay:1});
+    }, 1000);
+  })
+</script>
+
+
+
+<style lang="scss">
+  /** Include */
+  @import '/assets/scss/partials/variables';
+
+  #loader{
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      flex-direction: column;
+      height:100vh;
+      position: fixed;
+      z-index:100;
+      font-family: $font2;
+      background-color: #202020;
+      color: $text-color;
+      font-size: 70px;
+      width: 100%;
+  }
 </style>
