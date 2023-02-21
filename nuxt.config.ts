958ed17d7fd6d295bf6011a074bc62a05ad20d06
Thomas JACQUEY <thomas@webcd.fr>
2023-02-21 17:13:16 +0100

    fix: viewport mobile

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,7 @@ export default defineNuxtConfig({
     ssr: false,
     app:{
         head: {
-            viewport: 'width=device-width, initial-scale=1.0',
+            viewport: 'width=device-width, initial-scale=1.0, maximum-scale=1.0',
         }
     },
     build: {
