9ae3745345a4c62c2f5b1d50bd23ddf6df1e643f
Thomas JACQUEY <thomas@webcd.fr>
2023-08-25 14:09:14 +0200

    chore: add comment

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,19 +1,18 @@
 <script setup>
-  import { state, menuTrait } from "../composables/menu"
+  const { t } = useI18n();
 
+  // Intitializing header project
   useHead({
-    title: 'Jacquey Thomas - Chef de projet - Lead Dev - Dev',
-    // or, instead:
-    // titleTemplate: (title) => `My App - ${title}`,
+    title: t('layout.title'),
     charset: 'utf-8',
     meta: [
-      { name: 'description', content: 'My amazing site.' }
+      { name: 'description', content: t('layout.seo.description') }
     ],
     link: [
     //  { rel: 'stylesheet', href: 'https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css' }
     ],
     script: [
-       { hid: 'particles', src: 'https://cdnjs.cloudflare.com/ajax/libs/particlesjs/2.2.3/particles.min.js', defer: true }
+    //   { hid: 'particles', src: '', defer: true }
     ],
     bodyAttrs: {
       class: 'tomsjac'
@@ -22,24 +21,19 @@
 </script>
 
 <template>
+  <!-- Loading component -->
   <PartialsLoader></PartialsLoader>
+  <!-- Mouse customization component -->
   <PartialsCursor></PartialsCursor>
 
-   <!--
-    <nav>
-      <ul class="nav nav-pills">
-        <li class="nav-item" v-for="item, index in state.menu">
-          <NuxtLink class="nav-link" :class='{"active": item.isActive}' :key="'menu'+index" @click='menuTrait().handleNavClick(index)' :to="item.to">{{item.title}}</NuxtLink>
-        </li>
-      </ul>      
-    </nav>
-  -->
-
     <div class="content">
+      <!-- Particle component -->
       <PartialsParticles></PartialsParticles>
+      <!-- Content -->
       <slot />
     </div>
 
+    <!-- Profile image -->
     <div class="profile-image">
         <img src="/img/man.png" alt="profile">
     </div>
