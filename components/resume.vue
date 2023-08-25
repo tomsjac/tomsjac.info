a78c883134760043b51d9ccb1d6456ce889ff165
Thomas JACQUEY <thomas@webcd.fr>
2023-08-25 14:24:52 +0200

    feat: Add translate and comment

--- components/resume.vue
+++ components/resume.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="icons-box">
-        Pour mieux me <span class="color">connaitre</span>
+        {{ $t('section.resume.title') }} <span class="color">{{ $t('section.resume.titleImportantItem') }}</span>
         <span class="icons-box-list withLink">
             <PartialsIcon  v-for="icon in iconList" :data="icon" section="resume"></PartialsIcon>
         </span>
@@ -9,32 +9,34 @@
 
 <script setup lang="ts">
    import { menuModel } from  "@/composables/dataModel/menuModel"
+   const { t } = useI18n();
 
-    const iconList = [
+   // List of information to display as icons
+   const iconList = [
         { 
             name: 'linkedin',
-            tooltip: {title : 'Profil linkedin', content:'Retrouvez tous les informations sur mon parcours, mes formations, et mes attentes'},
+            tooltip: {title : t('section.resume.icons.linkedin.title'), content: t('section.resume.icons.linkedin.desc')},
             link: 'http://fr.linkedin.com/in/tomsjac',
             color: '#0A66C2',
             animation : 'tada'
         },
         { 
             name: 'resume',
-            tooltip: {title : 'Curriculum vitae', content:'Gardez au chaud, ce beau PDF'},
-            link: 'www.cv.fr',
+            tooltip: {title : t('section.resume.icons.resume.title'), content: t('section.resume.icons.resume.desc')},
+            link: 'https://github.com/tomsjac/resume.tomsjac.info/blob/089692182e36049bc97a3ae134852166bb779cf7/Thomas_JACQUEY_Resume.pdf',
             color: '#F48225',
             animation : 'pulse'
         },
         { 
             name: 'twitter',
-            tooltip: {title : 'Twitter', content:'Compte Twitter pour me tenir à jour des nouveautés (Dev, Tech, Actualité)'},
+            tooltip: {title : t('section.resume.icons.twitter.title'), content: t('section.resume.icons.twitter.desc')},
             link: 'https://twitter.com/tomsjac',
             color: '#5DA9DD',
             animation : 'jello'
         },
         { 
             name: 'github',
-            tooltip: {title : 'GitHub', content :'Mon dépot public pour mes expériences, mes tests ...'},
+            tooltip: {title : t('section.resume.icons.github.title'), content: t('section.resume.icons.github.desc')},
             link: 'https://github.com/tomsjac/tomsjac.info',
             color: '#7067E6',
             animation : 'rubberBand'
--- i18n.config.ts
+++ i18n.config.ts
@@ -15,6 +15,16 @@ export default defineI18nConfig(() => ({
                     lastname: 'Jacquey',
                     firstname: 'Thomas',
                     jobs: {lead: 'Lead dev.', dev: 'Développeur.', projectManager: 'Chef de projet.'}
+                },
+                resume: {
+                    title: 'Pour mieux me',
+                    titleImportantItem: 'connaitre',
+                    icons: {
+                        'linkedin': {title:'Profil linkedin', desc:'Découvrez mon parcours, mes formations et mes perspectives professionnelles.'},
+                        'resume': {title:'Curriculum vitae', desc:'N\'oubliez pas d\'enregistrez ce PDF.'},
+                        'twitter': {title:'Twitter', desc:'Pour rester informé des dernières tendances tech et dev.'},
+                        'github': {title:'GitHub', desc:'Découvrez mon espace : expérimentations, tests et bien d\'autres.'},
+                    }
                 }
             }
         }
