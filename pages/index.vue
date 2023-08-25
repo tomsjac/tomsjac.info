d9cd45dff185ed7ad8943dcde7a46e2a5835cc72
Thomas JACQUEY <thomas@webcd.fr>
2023-08-25 14:08:47 +0200

    feat: Add translate and clean

--- pages/index.vue
+++ pages/index.vue
@@ -1,21 +1,20 @@
+<!--
+  Single page composition
+-->
 <template>
+    <!-- Profile -->
     <About></About>
-    
+
+    <!-- Web link about me -->
     <Resume></Resume>
 
     <img src="/img/separator_mini.png" class='separator' >                 
-    
+
+    <!-- My skills  -->
     <Skills></Skills>
 
-    <img src="/img/separator_mini.png" class='separator' >                 
+    <img src="/img/separator_mini.png" class='separator' >
 
+    <!-- My hobbies  -->
     <Hobbies></Hobbies>
-</template>  
-  
-<script setup>
-    import { ref } from 'vue'
-</script>
-
-<style>
-
-</style>
+</template>
